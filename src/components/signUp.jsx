95b1cd3e947e6cf2a31f50e8cddbd090679580a5
Radosław Urban <urban_radoslaw@o2.pl>
2021-05-28 11:39:39 +0200

    PR_L12

--- src/components/signUp.jsx
+++ src/components/signUp.jsx
@@ -1,14 +1,20 @@
 import React, {Component} from "react";
 const axios = require('axios');
 
-class LoginForm extends Component {
+class SignUpForm extends Component {
 
     state = {
         account: {
             username: "",
+            email: "",
             password: ""
         },
         errors: {}
+    };
+
+    handleChangeRoute = () => {
+        this.props.history.push('/login');
+        window.location.reload();
     };
 
     validate = () => {
@@ -17,6 +23,9 @@ class LoginForm extends Component {
         const {account} = this.state;
         if (account.username.trim() === '') {
             errors.username = 'Username is required!';
+        }
+        if (account.email.trim() === '') {
+            errors.password = 'Email is required!';
         }
         if (account.password.trim() === '') {
             errors.password = 'Password is required!';
@@ -32,11 +41,14 @@ class LoginForm extends Component {
         this.setState({errors: errors || {}});
         if (errors) return;
 
+        console.log(this.state)
+
         axios({
             method: 'post',
-            url: 'http://localhost:3001/api/user/auth',
+            url: 'http://localhost:3001/api/user/create',
             data: {
-                login: this.state.account.username,
+                name: this.state.account.username,
+                email: this.state.account.email,
                 password: this.state.account.password
             }
         }).then((response) => {
@@ -56,18 +68,13 @@ class LoginForm extends Component {
         this.setState({account});
     };
 
-    handleChangeRoute = () => {
-        this.props.history.push('/');
-        window.location.reload();
-    };
-
     render() {
         return (
             <div>
-                <h1>Login</h1>
+                <h1>Sign Up</h1>
                 <form onSubmit={this.handleSubmit}>
                     <div className="form-group">
-                        <label htmlFor="username">Email address</label>
+                        <label htmlFor="username">Name</label>
                         <input value={this.state.account.username}
                                name="username"
                                onChange={this.handleChange}
@@ -78,6 +85,19 @@ class LoginForm extends Component {
                                placeholder="Username"/>
                         {this.state.errors.username &&
                         <div className="alert alert-danger">{this.state.errors.username}</div>}
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email">Email address</label>
+                        <input value={this.state.account.email}
+                               name="email"
+                               onChange={this.handleChange}
+                               type="email"
+                               className="form-control"
+                               id="email"
+                               aria-describedby="emailHelp"
+                               placeholder="Email"/>
+                        {this.state.errors.email &&
+                        <div className="alert alert-danger">{this.state.errors.email}</div>}
                     </div>
                     <div className="form-group">
                         <label htmlFor="password">Password</label>
@@ -91,7 +111,7 @@ class LoginForm extends Component {
                         {this.state.errors.password &&
                         <div className="alert alert-danger">{this.state.errors.password}</div>}
                     </div>
-                    <button type="submit" className="btn btn-primary">Login</button>
+                    <button type="submit" className="btn btn-primary">SignUp</button>
                 </form>
 
             </div>
@@ -99,4 +119,4 @@ class LoginForm extends Component {
     }
 }
 
-export default LoginForm;
+export default SignUpForm;
